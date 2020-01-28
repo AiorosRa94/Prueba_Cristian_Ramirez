20827583a4e747a2e98fdce48d39f57ecf90da5c
Cristian Ramírez Sánchez <isc.ramirez.sanchez@gmail.com>
2020-01-28 10:08:05 -0600

    Initial Commit

---
+++ Prueba_Cristian_Ramirez/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prueba_Cristian_Ramirez
+//
+//  Created by Abraham Figueroa Reyes on 28/01/20.
+//  Copyright © 2020 CristianRamirezSanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
